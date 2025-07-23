e9636b04e034e606b7e6c7c19460c2560cd99f50
Shevikina <shevik.vlog@gmail.com>
2025-07-23 18:09:41 +0300

    Добавлен кастомный класс

---
+++ app/src/main/java/com/example/customviewexample/ShapeView.kt
@@ -0,0 +1,147 @@
+package com.example.customviewexample
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.MotionEvent.ACTION_UP
+import android.view.View
+import android.widget.Toast
+import androidx.core.graphics.toColorInt
+import kotlin.random.Random
+
+class ShapeView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attributeSet, defStyleAttr) {
+
+    private var defaultColor = DEFAULT_SHAPE_COLOR
+    private var shapeColors: List<Int> = emptyList()
+    private val shapes = mutableListOf<ShapeInfo>()
+
+    private val shapePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = TEXT_SIZE
+        color = TEXT_COLOR
+        typeface = Typeface.DEFAULT_BOLD
+    }
+
+    init {
+        val typeArray = context.theme.obtainStyledAttributes(
+            attributeSet,
+            R.styleable.ShapeView,
+            defStyleAttr, 0
+        )
+        defaultColor = typeArray.getColor(
+            R.styleable.ShapeView_shv_defaultColor,
+            DEFAULT_SHAPE_COLOR
+        )
+        typeArray.recycle()
+    }
+
+    fun setIntColors(colors: List<Int>) {
+        shapeColors = colors
+    }
+
+    fun setHexColors(colors: List<String>) {
+        shapeColors = colors.map { it.toColorInt() }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.action == ACTION_UP) {
+            addRandomShape(event.x, event.y)
+            performClick()
+            return true
+        }
+        return super.onTouchEvent(event)
+    }
+
+    private fun addRandomShape(x: Float, y: Float) {
+        shapes.add(
+            ShapeInfo(
+                type = ShapeType.entries.toTypedArray().random(),
+                color = shapeColors.randomOrNull() ?: defaultColor,
+                size = Random.nextDouble(MIN_SHAPE_SIZE, MAX_SHAPE_SIZE).toFloat(),
+                cx = x,
+                cy = y
+            )
+        )
+
+        if (shapes.count() >= 10) clearShapes()
+
+        invalidate()
+    }
+
+    private fun clearShapes() {
+        shapes.clear()
+        Toast.makeText(context, "Game over", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        canvas.drawText("Фигур: ${shapes.count()}", TEXT_HEIGHT, TEXT_WIDTH, textPaint)
+
+        shapes.forEach {
+            shapePaint.color = it.color
+            canvas.drawShape(it)
+        }
+    }
+
+    private fun Canvas.drawShape(shape: ShapeInfo) {
+        when (shape.type) {
+            ShapeType.CIRCLE -> {
+                drawCircle(
+                    shape.cx,
+                    shape.cy,
+                    shape.size / 2,
+                    shapePaint
+                )
+            }
+
+            ShapeType.SQUARE -> {
+                drawRect(
+                    shape.rect(),
+                    shapePaint
+                )
+            }
+
+            ShapeType.ROUNDED_SQUARE -> {
+                drawRoundRect(
+                    shape.rect(),
+                    RECT_RADIUS,
+                    RECT_RADIUS,
+                    shapePaint
+                )
+            }
+        }
+    }
+
+    private fun ShapeInfo.rect(): RectF {
+        val halfSize = size / 2
+
+        return RectF(
+            cx - halfSize,
+            cy - halfSize,
+            cx + halfSize,
+            cy + halfSize
+        )
+    }
+
+    companion object {
+        const val DEFAULT_SHAPE_COLOR = Color.GREEN
+        const val MIN_SHAPE_SIZE = 20.0
+        const val MAX_SHAPE_SIZE = 50.0
+        const val RECT_RADIUS = 15f
+
+        const val TEXT_COLOR = Color.BLACK
+        const val TEXT_SIZE = 48f
+        const val TEXT_HEIGHT = 20f
+        const val TEXT_WIDTH = 60f
+    }
+}
