38f53d3bce66bd47d47ff673a28fcb98dcd79eea
Shevikina <shevik.vlog@gmail.com>
2025-07-23 18:02:48 +0300

    Добавлены типы фигур и модель данных фигуры

---
+++ app/src/main/java/com/example/customviewexample/ShapeInfo.kt
@@ -0,0 +1,9 @@
+package com.example.customviewexample
+
+data class ShapeInfo(
+    val type: ShapeType,
+    val color: Int,
+    val size: Float,
+    val cx: Float,
+    val cy: Float
+)
---
+++ app/src/main/java/com/example/customviewexample/ShapeType.kt
@@ -0,0 +1,7 @@
+package com.example.customviewexample
+
+enum class ShapeType {
+    CIRCLE,
+    SQUARE,
+    ROUNDED_SQUARE
+}
